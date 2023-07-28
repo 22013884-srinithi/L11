94d549297bee64e4a88ff3291570b6b8b3793b78
Muhammad Khalis <Muhammad Khalis@LAPTOP-UHPD97PS>
2023-07-28 19:02:52 +0800

    C206_CaseStudy: Rate methods - Khalis
    Rate Class - Khalis

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -1,11 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class C206_CaseStudy {
-	
-	
 
-	public static void main(String[] args) {
-		
+	// Static list to store all rates
+	private static List<Rate> rateList = new ArrayList<>();
 
+	// Static list to store all transactions 
+	private static List<Transactions> transactionList = new ArrayList<>();
+
+	// Method to add a new rate
+	public static void addRate(String currencyCode, double exchangeRate) {
+		Rate newRate = new Rate(currencyCode, exchangeRate);
+		rateList.add(newRate);
 	}
 
+	// Method to view all rates
+	public static List<Rate> getAllRates() {
+		return rateList;
+	}
+
+	// Method to delete an existing rate
+	public static boolean deleteRate(String currencyCode) {
+		for (Rate rate : rateList) {
+			if (rate.getCurrencyCode().equals(currencyCode)) {
+				rateList.remove(rate);
+				return true; // Rate deleted successfully
+			}
+		}
+		return false; // Rate with given currencyCode not found
+	}
+
+	public static void main(String[] args) {
+		// Main method and other code here...
+	}
 }
+
+	
--- C206_CaseStudy/src/Rate.java
+++ C206_CaseStudy/src/Rate.java
@@ -1,38 +1,12 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class Rate {
+	
+	
     private String currencyCode;
     private double exchangeRate;
-
-    // Static list to store all rates (could be replaced with a database in a real application)
-    private static List<Rate> rateList = new ArrayList<>();
 
     public Rate(String currencyCode, double exchangeRate) {
         this.currencyCode = currencyCode;
         this.exchangeRate = exchangeRate;
-    }
-
-    // Method to add a new rate
-    public static void addRate(String currencyCode, double exchangeRate) {
-        Rate newRate = new Rate(currencyCode, exchangeRate);
-        rateList.add(newRate);
-    }
-
-    // Method to view all rates
-    public static List<Rate> getAllRates() {
-        return rateList;
-    }
-
-    // Method to delete an existing rate
-    public static boolean deleteRate(String currencyCode) {
-        for (Rate rate : rateList) {
-            if (rate.getCurrencyCode().equals(currencyCode)) {
-                rateList.remove(rate);
-                return true; // Rate deleted successfully
-            }
-        }
-        return false; // Rate with given currencyCode not found
     }
 
     // Getters for attributes
@@ -44,6 +18,7 @@ public class Rate {
         return exchangeRate;
     }
 
+    @Override
     public String toString() {
         return "Currency Code: " + currencyCode + ", Exchange Rate: " + exchangeRate;
     }
